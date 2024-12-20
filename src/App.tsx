7fb043b42eee67561c158524379559a58cef4035
it's -Abhi <abhishekmakala18@gmail.com>
2024-12-20 12:54:44 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+//
+//import Product from "./components/Product";
+import EmpSal from "./components/EmpSal";
+import EmpApi from "./components/EmpApi";
+import Wonders from "./components/Wonders"
+export default function App() {
+  return (
+    <div>
+      {/* <Product/> */}
+      {/* <EmpSal /> */}
+      {/* <EmpApi/> */}
+      <Wonders/>
+
+    </div>
+  );
+}
